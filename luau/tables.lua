7c3d05cd4b92ba6e96bdbb515c564acd10f354b2
@Ceo-Sammarco <noah.kd@gmail.com>
2023-12-20 01:40:36 -0300

    @Ceo-Sammarco - qua 20 dez 2023

---
+++ luau/tables.lua
@@ -0,0 +1,19 @@
+
+
+local list = {
+	"Débora",
+	"Acácio",
+	"Anselmo",
+	"Gabriel",
+	"Rafael",
+	"Beatriz",
+	"Leonardo",
+	"Miguel",
+	"Felipe",
+	"Noely",
+	"Bernardo",
+}
+
+for index, valor  in pairs( list )  do
+	print( "Nome: "..valor.." - Indice: "..index )
+end
---
+++ notes.md
@@ -0,0 +1,3 @@
+
+
+rbxopenscript://www.example.com/dummy?AE2A-67C8195ABD35%7D&gst=1#29
